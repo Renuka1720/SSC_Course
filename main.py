26706d52cc24815b79d48278eef343a9009327c3
julehe <julian.heidecke@gmail.com>
2025-06-25 11:29:44 +0200

    added main.py with some intial code

---
+++ main.py
@@ -0,0 +1,8 @@
+import transform as trf
+import pandas as pd
+
+if __name__ == "__main__":
+    npop = pd.read_csv("data/npop.t", sep=r"\s+")
+    npop = trf.filter_col_by_var(npop)
+    test = trf.correlation_pairs(npop, exclude="time")
+    print(test)
